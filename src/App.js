9a9b7c06bfe604e90924e38364e91a1c6cb16ad7
SeyunKim <coolseyun@gmail.com>
2023-08-30 14:27:08 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Homework0 complete
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
